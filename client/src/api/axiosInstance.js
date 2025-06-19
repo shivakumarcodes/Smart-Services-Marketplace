51002948a7ca6c123209313508b8387495e428dd
G SHIVA KUMAR <galishivakumar2002@gmail.com>
2025-06-19 21:50:50 +0530

    New Backend Deploy

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://shiva-smart-services.onrender.com";
+export const BASE_URL = "https://smart-services-marketplace.onrender.com";
 // export const BASE_URL = "http://localhost:5000";
